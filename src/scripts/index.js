e9df9439a8e2808c7a4ad155920bdd84a0e7b7ca
FthRobby <rbysalam10@gmail.com>
2022-06-20 10:52:45 +0700

    fixing code error use eslint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,14 +3,14 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import '../styles/components/button.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import App from './views/app';
 import swRegister from './utils/sw-register';
 
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 AOS.init({
     disable: false,
